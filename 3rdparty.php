a1f132a3d0132cd28a37356344be5f49a05b3402
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2006-11-05 15:08:29 +0000

    Added a 3rd party apps/docs page
    git-svn-id: file:///openmsx-svn/code/htdocs/trunk@5849 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ 3rdparty.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <?php
+    $current = "3rd Party";
+    require "head.div";
+?>
+<body>
+  <?php require "title.div"; ?>
+  <?php require "navigation.div"; ?>
+  
+  <div id="content">
+    <h1>3rd Party</h1>
+    <p>
+      On this page you will find some 3rd party material that is related to
+      openMSX, but not written by the openMSX authors. This also means that
+      the openMSX authors do not support it, nor keep it up to date, as it is
+      unofficial. But it can be very interesting for our users and we also
+      like it very much that people write applications around openMSX or write
+      extra documentation. Keep up the good work, people!  
+    </p>
+    <p>
+      Oh, and if there's some interesting stuff we missed, please let us know!
+    </p>
+    <h2>Launchers</h2>
+    <dl>
+      <dt>
+        <a href="http://www5f.biglobe.ne.jp/~nekocan/group_soft/ht_soft.html">NekoLauncher openMSX</a>
+      </dt>
+      <dd>
+        Because the old style Catapult does not run (properly) on Mac,
+	an active Japanese emulator user/programmer wrote an openMSX
+	variant of his NekoLauncher series. It loos like a simple launcher,
+	but also very user friendly.
+      </dd>
+    </dl>
+    
+    <h2>Documentation</h2>
+    <dl>
+      <dt>
+        <a href="http://www.msxgamesbox.com/Sections/emumac/openmsx.php">openMSX Compilation Guide for OS X</a>
+      </dt>
+      <dd>
+	This article on The MSX Games Box explains the compilation process of
+	openMSX on MacOS X in great detail.
+      </dd>      
+      <dt>
+        <a href="http://www.passionmsx.org/modules/smartsection/item.php?itemid=17">openMSX hyper facile pour Mac OS X</a>
+      </dt>
+      <dd>
+	This article (in French) on Passion MSX explains to install a binary
+	version of openMSX and NekoLauncher openMSX on your Mac.  
+      </dd>      
+    </dl> 
+
+   <h2>Miscellaneous</h2>
+    <dl>
+      <dt>
+        <a href="http://msx.jannone.org/openMSXcontrol/">OpenMSX Control Plugin for Gedit</a>
+      </dt>
+      <dd>
+        Rafael Jannone made an interesting thing: a plugin for Gedit, so that
+	you can easily test your MSX Basic programs in openMSX, while using
+	the Gedit text editor. He wrote a whole load of Python scripts to 
+	make this possible. They might be nice to reuse for other apps!
+      </dd>      
+    </dl> 
+  </div>
+  <div id="footer">
+    $Id: contact.php 331 2006-07-30 11:29:10Z manuelbi $
+  </div>
+</body>
+</html>
