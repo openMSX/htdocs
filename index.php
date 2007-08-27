cf4d2e11effd4ef8bd6d54ba423cea0e11472fb1
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2007-08-27 20:45:03 +0000

    Call for Win32 maintainer
    git-svn-id: file:///openmsx-svn/code/htdocs/trunk@6858 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- index.php
+++ index.php
@@ -11,6 +11,13 @@
   <div id="content">
     <h1>News</h1>
      <h2>
+      <span class="date">2007 August 27</span>
+      No Windows support anymore?? 
+    </h2>
+    <p>
+    We've said it before: we need someone in our team to help us maintain the Windows port of openMSX. At the moment there is no one who provides us structural support for the Windows port. Without a person who can very regularly build and test openMSX and help in case of (rare) Windows specific issues, we cannot maintain the Windows port any longer. We may find a way to build the next release, but even that is uncertain; if we can manage, it will most likely be released (much) later than the Source and Mac versions. If you care and you want to help: please <a href="http://openmsx.sourceforge.net/contact.php">contact us</a> as soon as possible!
+    </p>
+     <h2>
       <span class="date">2007 April 15</span>
       openMSX 0.6.2 released
     </h2>
