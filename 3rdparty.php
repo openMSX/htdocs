9dadb547ba4364f2e9baa07991fe416c2d187aeb
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2024-09-24 23:12:20 +0200

    Updates, thanks to Bart Bonamie for all suggestions.
    More to follow, I hope!

--- 3rdparty.php
+++ 3rdparty.php
@@ -11,77 +11,70 @@
   <div id="content">
     <h1>3rd Party</h1>
     <p>
-      On this page you will find some 3rd party material that is related to
-      openMSX, but not written by the openMSX authors. This also means that
-      the openMSX authors do not support it, nor keep it up to date, as it is
-      unofficial. But it can be very interesting for our users and we also
-      like it very much that people write applications around openMSX or write
-      extra documentation. Keep up the good work, people!
-    </p>
-    <p>
-      Oh, and if there's some interesting stuff we missed, please let us know!
+      On this page, you’ll find third-party material related to openMSX.
+      While these resources are not authored or maintained by the openMSX
+      team, they may still offer valuable insights and enhancements for our
+      users. We greatly appreciate the community’s efforts in creating
+      applications and additional documentation around openMSX. Keep up the
+      good work, people, and <a href="https://www.openmsx.org/contact.html">
+      drop us a line</a> if you have a project you would like to see added here!
     </p>
     <h2>Launchers</h2>
     <dl>
       <dt>
-        <a href="http://msxlaunchers.info/">openMSX Launcher</a>
+        <a href="http://msxlaunchers.info/">openMSX Launcher</a> (unsecured link)
       </dt>
       <dd>
         An openMSX version of the blueMSX Launcher, mostly targeted at
         launching games. It supports screenshots for games and a large number
         of languages. Multiplatform: Windows, Mac, Linux, BSD.
+        Development has stopped and development efforts are now concentrated on
+        Novo Player.
       </dd>
+        <dt>
+          <a href="http://msxlaunchers.info">Novo Player</a> (unsecured link)
+        </dt>
+        <dd>
+          Novo Player is a new, versatile, multi-platform front-end designed
+          primarily for openMSX and is currently very much under active
+          development. Currently, the <a href="http://msxlaunchers.info/download.html">
+          latest version is 1.10.1</a>.
+        </dd>
       <dt>
-        <a href="http://nekocanmac.seesaa.net/">NekoLauncher openMSX</a>
+        <a href="http://nekocanmac.seesaa.net/">NekoLauncher openMSX</a> (unsecured link)
       </dt>
       <dd>
         Because the old style Catapult does not run (properly) on Mac,
-        an active Japanese emulator user/programmer wrote an openMSX
-        variant of his NekoLauncher series. It's not only getting more and
+        an active Japanese emulator user/programmer wrote an
+        <a href="https://nekocanmac.up.seesaa.net/image/NekoLauncheropenMSX_v15.zip">
+        openMSX variant of his NekoLauncher</a> series. It's not only getting more and
         more advanced, but also seems to be very user friendly.
       </dd>
       <dt>
-        <a href="http://cesco.it/il-mio-software-gratuito/openmsx-peashooter/">openMSX Peashooter</a> (unfortunately the site seems to have disappeared)
+        <a href="https://web.archive.org/web/20110123115755/http://cesco.it/openmsx-peashooter/">openMSX Peashooter</a>
       </dt>
       <dd>
         For the same reason, Cesco made another launcher for openMSX on OS X.
-        It seems to be focused mainly on games and features image previews.
-      </dd>
-    </dl>
-
-    <h2>Documentation</h2>
-    <dl>
-      <dt>
-        <a href="http://www.msxgamesbox.com/passion/msxgamesbox/Sections/emumac/openmsx.php">openMSX Compilation Guide for OS X</a>
-      </dt>
-      <dd>
-        This article on The MSX Games Box (now part of Passion MSX) explains
-        the compilation process of openMSX on macOS in great detail, however,
-        it is now mostly outdated.
-      </dd>
-      <dt>
-        <a href="http://www.passionmsx.org/modules/smartsection/item.php?itemid=26">Hyper-easy openMSX for Mac OS X</a>
-      </dt>
-      <dd>
-        This article on Passion MSX explains to install a binary
-        version of openMSX and NekoLauncher openMSX on your Mac. It is probably
-        quite outdated, though, as it was written in 2006.
+        It seemed to be focused mainly on games and features image previews.
+        Unfortunately, the site and download links have disappeared.
       </dd>
     </dl>
 
    <h2>Miscellaneous</h2>
     <dl>
       <dt>
-        <a href="http://openmsx.fixato.net/">openMSX Daily Builds</a>
+        <a href="https://openmsx.fixato.net/">openMSX Daily Builds</a>
       </dt>
       <dd>
         FiXato created a great web site which provides you with daily built
         binaries of openMSX. Very much recommended if you want to try out
         development versions of openMSX without compiling them yourself!
         Currently supporting Windows, macOS, Android and OpenDingux platforms.
+        Unfortunately, the disk space seems to have run full and it no longer
+        hosts recent builds.
       </dd>
       <dt>
-        <a href="http://msx.jannone.org/openMSXcontrol/">OpenMSX Control Plugin for Gedit</a>
+        <a href="https://msx.jannone.org/openMSXcontrol/">OpenMSX Control Plugin for Gedit</a>
       </dt>
       <dd>
         Rafael Jannone made an interesting thing: a plugin for Gedit, so that
